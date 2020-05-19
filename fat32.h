d2cc6e7fbdec4f2ea6b336fef88e51ab9464d116
Bjørn Brodtkorb <bjorn.thoralf@outlook.com>
2020-05-19 18:58:25 +0200

    Updated copyright format

--- fat32.h
+++ fat32.h
@@ -1,10 +1,9 @@
 //------------------------------------------------------------------------------
 // Copyright (c) 2020 Bjørn Brodtkorb
 //
-// This software is provided without warranty of any kind.
-// Permission is granted, free of charge, to copy and modify this
-// software, if this copyright notice is included in all copies of
-// the software.
+// This software is provided without warranty of any kind. Permission is 
+// granted, free of charge, to copy and modify this software, if this copyright
+// notice is included in all copies of the software.
 //------------------------------------------------------------------------------
 
 
